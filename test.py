aa4c5f3d0635a4b318a7188978bc5598e075d6e2
Felix Mönckemeyer <felix.moenckemeyer@rwth-aachen.de>
2018-04-24 15:30:34 +0200

    ssad

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import statistics
+import numpy as np
+import matplotlib.pyplot as plt
+
 file = open("Europarl.txt", "r", encoding="utf8")
 n = 1
 
@@ -13,6 +16,7 @@ for chunk in iter(lambda: file.read(n), ''):
     countChar += 1
     if countChar % 1000000 == 0:
         print(countChar/1000000)
+        break
 
     if chunk == " ":
         wCount += 1
@@ -40,3 +44,20 @@ print(statistics.variance(wordArray))
 print("Mean / Variance of Sentences")
 print(statistics.mean(sentenceArray))
 print(statistics.variance(sentenceArray))
+
+maxSentenceLength = max(sentenceArray)
+maxWordLength = max(wordArray)
+print(maxWordLength)
+print(maxSentenceLength)
+
+distSentence = [0] * (maxSentenceLength + 1)
+distWord = [0] * (maxWordLength + 1)
+
+for s in sentenceArray:
+    distSentence[s] += 1
+
+for w in wordArray:
+    distWord[w] += 1
+
+for d in distWord:
+    print(d)
