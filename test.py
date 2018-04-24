ff07f4d5a134fb97d59a865cccea9ec97f764a14
Felix Mönckemeyer <felix.moenckemeyer@rwth-aachen.de>
2018-04-24 17:20:43 +0200

    plot rdy

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import statistics
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import *
 
 file = open("Europarl.txt", "r", encoding="utf8")
 n = 1
@@ -11,11 +11,12 @@ wLength = 0
 wCount = 0
 countChar = 0
 endOfSentence = False
+calcGraph = 0
 
 for chunk in iter(lambda: file.read(n), ''):
     countChar += 1
-    if countChar % 1000000 == 0:
-        print(countChar/1000000)
+    if countChar % 10000000 == 0:
+        calcGraph = countChar/1000000
         break
 
     if chunk == " ":
@@ -59,5 +60,26 @@ for s in sentenceArray:
 for w in wordArray:
     distWord[w] += 1
 
-for d in distWord:
-    print(d)
+distSentence = distSentence[:150]
+distWord = distWord[:20]
+
+xlocations = np.array(range(len(distWord)))+0.5
+width = 1
+bar(xlocations, distWord, width=width)
+yticks(np.arange(0, max(distWord), 10000*calcGraph))
+xticks(xlocations + width/2, np.arange(0, len(distWord) + 1, 1))
+title("Word Length Distribution")
+gca().get_xaxis().tick_bottom()
+gca().get_yaxis().tick_left()
+
+#np.arange(0, len(distWord) + 1, 1
+show()
+
+#xlocations = np.array(range(len(distSentence)))+0.5
+#width = 1
+#bar(xlocations, distSentence, width=width)
+#yticks(np.arange(0, max(distSentence), 15*calcGraph))
+#xticks(np.arange(0, len(distSentence) + 1, 20))
+#title("Sentence Length Distribution")
+#gca().get_xaxis().tick_bottom()
+#gca().get_yaxis().tick_left()
